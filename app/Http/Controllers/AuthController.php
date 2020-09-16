4dae147a416be80fc16be6426b1e3ba1f39a0750
muhammadzakinudin <zakinugraha001@gmail.com>
2020-09-16 20:45:57 +0700

    Creating Login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use App\User;
 
 class AuthController extends Controller
@@ -15,7 +16,17 @@ class AuthController extends Controller
     public function postLogin(Request $request)
     {
     	$data = $request->all();
-    	dd($data);
+        $api_url = config('api.api_url').'/login';
+        // dd($api_url);
+        $responseDetail = Http::withHeaders([
+            'content-type' => 'application/json'
+        ])->post($api_url, [
+            'email' => $data['email'],
+            'password' => $data['password']
+        ]);
+        $status_code = $responseDetail->getStatusCode();
+        $result = $responseDetail->json();
+    	dd($result);
     }
 
     public function getRegister()
---
+++ config/api.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'api_url' => env('API_URL', 'http://api.zakinugraha.com/'),    
+];
